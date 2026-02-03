5374455c91545ec3fbeef829070958c9e3fd89cb
Lord-Abiolla <abiollajames@protonmail.com>
2026-02-03 06:52:34 +0300

    Update job page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
 import JobBoardDashboard from "@/components/dashboard/JobBoardDashboard";
-import { serverFetch } from "@/lib/server-api";
 
-export default async function Home() {
-  const jobs = await serverFetch("/jobs/");
-
-  return <JobBoardDashboard initialJobs={jobs} />;
+export default function Home() {
+  return <JobBoardDashboard />;
 }
